efc52a9d2e0741ff02cb53d78169f9db939c432b
Max SCHMELLER <max.schmeller@tier4.jp>
2025-10-28 16:10:45 +0900

    hotfix: behave correctly when points with times larger than the scan period exist
    GLIM cuts IMU sequences according to the period of LiDAR scans.
    For some LiDAR configurations, it might happen that the span of point
    timestamps in one scan is larger than its publishing period.
    
    For example, a 360deg Hesai LiDAR decoded with Nebula will have points
    from a span of up to 120ms for a 10Hz frequency (100ms period), due to
    angle and fire time correction.
    
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- src/glim/preprocess/cloud_preprocessor.cpp
+++ src/glim/preprocess/cloud_preprocessor.cpp
@@ -121,7 +121,8 @@ PreprocessedFrame::Ptr CloudPreprocessor::preprocess_impl(const RawPoints::Const
   for (int i = 0; i < frame->size(); i++) {
     const bool is_finite = frame->points[i].allFinite();
     const double squared_dist = (Eigen::Vector4d() << frame->points[i].head<3>(), 0.0).finished().squaredNorm();
-    if (squared_dist > squared_distance_near_thresh && squared_dist < squared_distance_far_thresh && is_finite) {
+    const bool is_below_100ms = frame->times[i] < 100e-3;
+    if (squared_dist > squared_distance_near_thresh && squared_dist < squared_distance_far_thresh && is_finite && is_below_100ms) {
       indices.push_back(i);
     }
   }
